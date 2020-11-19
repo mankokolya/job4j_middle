6eb687ffb072257e07481bc07ed42f1336ca6325
manko kolya <mankokolya@gmail.com>
2020-11-19 11:40:54 +0200

    training daemon

---
+++ concurrency/src/main/java/ru/job4j/concurrency/training/dataloader/NetworkConnectionsLoader.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrency.training.dataloader;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class NetworkConnectionsLoader implements Runnable {
+    @Override
+    public void run() {
+        System.out.printf("Beginning network data sources loading: %s\n", new Date());
+        try {
+            TimeUnit.SECONDS.sleep(6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("NetworkData sources loading has finished: %s\n", new Date());
+    }
+}
